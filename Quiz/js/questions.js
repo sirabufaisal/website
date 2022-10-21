a463a8eb0dffa2be6bbbc3f153c92ed2a4fad13c
Mohammed Alqahtani <67894571+sirabufaisal@users.noreply.github.com>
2022-10-21 10:59:07 +0300

    Add files via upload

---
+++ Quiz/js/questions.js
@@ -0,0 +1,303 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "🙄 من انا ؟(تسخين)",
+    answer: "محمد",
+    options: [
+      "اسامة",
+      "محمد",
+      "عمر",
+      "بو قحط"
+      
+    ]
+  },
+  {
+    numb: 2,
+    question: "من اكثر واحد يجلق ؟ 🦾",
+    answer: "اسامة 😆",
+    options: [
+      "عمر 😃",
+      "بو قحط 🙂",
+      "اسامة 😆",
+      "سعيد 🤪"
+    ]
+  },
+  {
+    numb: 3,
+    question: "شخص يمتاز بطق الظهر 😣",
+    answer: "سعيد 🥱",
+    options: [
+      "بو قحط 🤯",
+      "اسامة 🤐",
+      "سعيد 🥱",
+      "محمد 😴"
+    ]
+  },
+    {
+    numb: 4,
+    question: "اكثر واحد يطبخ",
+    answer: "بو قحط 🙈",
+    options: [
+      "انا 😏",
+      "الطباخ 🙄",
+      "بو قحط 🙈",
+      "سعيد ✅"
+    ]
+  },
+    {
+    numb: 5,
+    question: "مشروب مفضل لمحمد ☕",
+    answer: "جاي حليب 🥰",
+    options: [
+      "كودرد 😂",
+      "قهوة 🤔",
+      "جاي حليب 🥰",
+      "موية 🤨"
+    ]
+  },
+  {
+    numb: 6,
+    question: "شخص يمتاز بصفة الجيزنه 😤",
+    answer: "عمر 🙄",
+    options: [
+      "عمر 🙄",
+      "اسامة 🤔",
+      "بو قحط 😕",
+      "انت 🤣"
+    ]
+  },
+  {
+    numb: 7,
+    question: "<img src=./js/media/download.jpg width='128' height='128'> \n وش الحيوان ذا",
+    answer: "الافن 🤢",
+    options: [
+      "عنكبوت 🕷",
+      "الافن 🤢",
+      "ضفضع 🐸",
+      "خروف 🐑"
+    ]
+  },
+  
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+/*
+{
+    numb: 8,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 9,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 10,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 11,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 12,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 13,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 14,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 15,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 16,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 17,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 18,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 19,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+  {
+    numb: 20,
+    question: "",
+    answer: "",
+    options: [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  */
+];
